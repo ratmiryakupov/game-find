fd8cbb3d638e40b06be119f5856185892e79ab3f
Ratmir Yakupov <wexanir@gmail.com>
2025-05-01 13:45:03 +0500

    fix bag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.ui.text.android)
-    implementation(libs.mobileads)
-    implementation(libs.play.services.ads.lite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
